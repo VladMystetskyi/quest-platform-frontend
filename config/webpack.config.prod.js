e6fca4f2750f348af18639dbc1931791cc89c0ed
mikxman <mikxman@gmail.com>
2016-11-17 00:41:32 +0200

    feat: add less loader

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -130,6 +130,10 @@ module.exports = {
         loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1&-autoprefixer!postcss')
         // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
       },
+      {
+        test: /\.less$/,
+        loader: "style!css!less"
+      },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
       {
