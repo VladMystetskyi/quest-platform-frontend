e3c9d16d9bc31b15abd3f67c42475e4426e4a1a3
mikxman <mikxman@gmail.com>
2016-11-17 02:12:50 +0200

    feat: add eslint for standard

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.less'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import RaisedButton from 'material-ui/RaisedButton'
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider>
+        <div className='App'>
+          <div className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className='App-intro'>
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <RaisedButton />
         </div>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </MuiThemeProvider>
     )
   }
 }
